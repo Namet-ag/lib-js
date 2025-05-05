9f81b89ec54ca0ad56924e430f2be138047366cc
Isaiah Meek <50851883+eyezahhhh@users.noreply.github.com>
2025-05-05 22:31:13 +1200

    update setup script

--- setup.js
+++ setup.js
@@ -1,3 +1,25 @@
 const FS = require("fs");
 
 FS.copyFileSync(".npmignore", "client/.npmignore");
+
+const packageJson = JSON.parse(FS.readFileSync("client/package.json", "utf-8"));
+
+packageJson.author = "eyezah";
+
+const BAD_KEYWORDS = [
+    "openapi-client",
+    "openapi-generator"
+];
+
+for (let keyword of BAD_KEYWORDS) {
+    const index = packageJson.keywords.indexOf(keyword);
+    if (index >= 0) {
+        packageJson.keywords.splice(index, 1);
+    }
+}
+
+packageJson.repository.url = "https://github.com/namet-ag/lib-js.git";
+
+packageJson.license = "MIT";
+
+FS.writeFileSync("client/package.json", JSON.stringify(packageJson, null, 2));
